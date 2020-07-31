bea8369c8b44640115735b3a61c32f7d299a3970
Toby Smith <tobysmith568@hotmail.co.uk>
2020-07-31 17:46:20 +0100

    Tidied up the redirect function

--- NewRedirect/Redirect.cs
+++ NewRedirect/Redirect.cs
@@ -1,9 +1,7 @@
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 
 namespace NewRedirect
@@ -17,9 +15,7 @@ namespace NewRedirect
         };
 
         [FunctionName("Redirect")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
-            ILogger log)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req)
         {
             string key = req.Query["key"];
 
