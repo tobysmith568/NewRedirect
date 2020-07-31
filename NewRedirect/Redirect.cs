75dbf9f2605093311a89088a322e11e30d588dd3
Toby Smith <tobysmith568@hotmail.co.uk>
2020-07-31 17:36:00 +0100

    Added the redirect function

---
+++ NewRedirect/Redirect.cs
@@ -0,0 +1,41 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace NewRedirect
+{
+    public static class Redirect
+    {
+        private static readonly Dictionary<string, string> Redirects = new Dictionary<string, string>
+        {
+            { "doc", "https://docs.google.com/document/u/0/create" },
+            { "sheet", "https://docs.google.com/spreadsheets/u/0/create" }
+        };
+
+        [FunctionName("Redirect")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string key = req.Query["key"];
+
+            if (key == null)
+            {
+                return new BadRequestResult();
+            }
+
+            string newURL = Redirects.GetValueOrDefault(key);
+
+            if (newURL == null)
+            {
+                return new BadRequestResult();
+            }
+
+            return new RedirectResult(newURL);
+        }
+    }
+}
