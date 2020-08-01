98114b6c69f489f4166a7ae49c8563e2ade79f11
Toby Smith <tobysmith568@hotmail.co.uk>
2020-08-01 17:19:03 +0100

    Added Google Slides

--- NewRedirect/Redirect.cs
+++ NewRedirect/Redirect.cs
@@ -11,7 +11,8 @@ namespace NewRedirect
         private static readonly Dictionary<string, string> Redirects = new Dictionary<string, string>
         {
             { "doc", "https://docs.google.com/document/u/0/create" },
-            { "sheet", "https://docs.google.com/spreadsheets/u/0/create" }
+            { "sheet", "https://docs.google.com/spreadsheets/u/0/create" },
+            { "slides", "https://docs.google.com/presentation/u/0/create" }
         };
 
         [FunctionName("Redirect")]
