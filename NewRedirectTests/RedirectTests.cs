8b88efef8041017219ca0ca20212d6d2da0888bc
Toby Smith <tobysmith568@hotmail.co.uk>
2020-08-01 20:01:07 +0100

    Added tests for Redirect

---
+++ NewRedirectTests/RedirectTests.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewRedirect.Tests
+{
+    [TestFixture]
+    public class RedirectTests
+    {
+        [TestCase("doc", ExpectedResult= "https://docs.google.com/document/u/0/create")]
+        [TestCase("sheet", ExpectedResult= "https://docs.google.com/spreadsheets/u/0/create")]
+        public string ValidRequestResultsInARedirect(string key)
+        {
+            var queryCollection = new Mock<IQueryCollection>();
+            queryCollection
+                .Setup(q => q["key"])
+                .Returns(key);
+
+            var request = new Mock<HttpRequest>();
+            request
+                .Setup(r => r.Query)
+                .Returns(queryCollection.Object);
+
+            var result = Redirect.Run(request.Object);
+
+            Assert.IsInstanceOf<RedirectResult>(result);
+
+            RedirectResult redirectResult = (RedirectResult)result;
+            return redirectResult.Url;
+        }
+
+        [Test]
+        public void NoKeyResultsInBadRequest()
+        {
+            var request = new Mock<HttpRequest>();
+            request
+                .Setup(r => r.Query)
+                .Returns(new QueryCollection());
+
+            var result = Redirect.Run(request.Object);
+
+            Assert.IsInstanceOf<BadRequestResult>(result);
+        }
+
+        [TestCase("")]
+        [TestCase("not a valid key")]
+        [TestCase("also not valid")]
+        public void IncorrectKeyResultsInBadRequest(string key)
+        {
+            var queryCollection = new Mock<IQueryCollection>();
+            queryCollection
+                .Setup(q => q["key"])
+                .Returns(key);
+
+            var request = new Mock<HttpRequest>();
+            request
+                .Setup(r => r.Query)
+                .Returns(queryCollection.Object);
+
+            var result = Redirect.Run(request.Object);
+
+            Assert.IsInstanceOf<BadRequestResult>(result);
+        }
+    }
+}
